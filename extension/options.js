d4a4315428176425f3c65249b3fa135e93ddaf94
Sam Hemann <sam.hemann@blueacorn.com>
2020-08-13 11:42:31 -0400

    added icon and fixed url matching

--- extension/options.js
+++ extension/options.js
@@ -1,8 +1,10 @@
-
+// Populate the text input with the current value
 chrome.storage.sync.get(['locationVal'], function (result) {
   console.log('Got location', result.locationVal)
   document.getElementById('locationInput').value = result.locationVal || ''
 })
+
+// Update the setting on save clicks
 document.getElementById('saveButton').addEventListener('click', function () {
   chrome.storage.sync.set({locationVal: document.getElementById('locationInput').value}, () => {
     console.log('New val', document.getElementById('locationInput').value)
